26f1039f1549e8532b76c764446a53831768d4b9
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-05-31 11:45:03 +0530

    feat:implement Short-Term memory for conversation history

--- main.py
+++ main.py
@@ -6,14 +6,28 @@ load_dotenv()
 
 client = genai.Client(api_key=os.getenv("GEMINI_API"))
 
+messages = []
+
 while  True:
 
     user_input= input("Enter Your  Prompt Here --->")
 
+    messages.append({
+
+        "role":"user",
+        "content":user_input
+
+    })
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=user_input
+        contents=list(map( lambda message: message["role"] + ":" + message["content"], messages))
     )
+
+    messages.append({
+        "role":"ai",
+        "content":response.text
+    })
 
     print(response.text)
 
