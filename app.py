c70f16115ef271f1a4c0a2807fd2462367a7f5bd
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-05-31 20:42:49 +0530

    feat: handle user input and store chat messages in session state

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 import streamlit as st
 
-messages = [
-    {
-        "role":"user",
-        "content":"Hello",
-    },{
-        "role":"ai",
-        "content":"Hi, How can I help you today ?"
-    }
-]
+if "messages" not in st.session_state:
+    st.session_state.messages=[]
 
-
-for message in messages:
+for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
 
-st.chat_input("Write prompt here ........")
+user_input = st.chat_input("Write prompt here ........")
+
+if user_input:
+
+    st.session_state.messages.append({"role": "user", "content" : user_input})
+
+    with st.chat_message("user"):
+        st.markdown(user_input)
